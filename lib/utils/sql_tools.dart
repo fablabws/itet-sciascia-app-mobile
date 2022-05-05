d9b748329ee8d668a4cb7f13e4b2b177a9928cf4
emanueledomingo <emanu.domingo@gmail.com>
2022-05-05 18:22:55 +0200

    aggiunto codice per creare una tabella

--- lib/utils/sql_tools.dart
+++ lib/utils/sql_tools.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart' as sql_lite;
+import 'package:path/path.dart';
 
 class SQLTools {
-  static Future<void> creaTabella(sql_lite.Database database) async {}
+  // questa funzione esegue una query che crea una tabella (verrà eseguito solo la prima volta)
+  static Future<void> creaTabella(sql_lite.Database database) async {
+    String sqlQuery = """CREATE TABLE progetti(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        titolo TEXT,
+        descrizione TEXT,
+        creato_a TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+    """;
+    await database.execute(sqlQuery);
+  }
 
-  static Future<sql_lite.Database> db() async {}
+  // questa funzione si connette ad un database e ce lo restituisce
+  static Future<sql_lite.Database> connetti() async {
+    return sql_lite.openDatabase(
+      join(await sql_lite.getDatabasesPath(), 'database_progetti.db'),
+      version: 1,
+      onCreate: (sql_lite.Database database, int version) async {
+        creaTabella(database);
+      },
+    );
+  }
 
   // legge tutti i progetti dal database
   static Future<List<Map<String, dynamic>>> leggiProgetti() async {}
