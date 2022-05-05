22fb0107d6a7dc175a8396b4b6841de8f1edbdba
emanueledomingo <emanu.domingo@gmail.com>
2022-05-05 17:41:24 +0200

    creata pagina "i miei progetti"

---
+++ lib/pages/my_projects.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class MyProjectPage extends StatefulWidget {
+  MyProjectPage({Key? key}) : super(key: key);
+
+  @override
+  _MyProjectPageState createState() => _MyProjectPageState();
+}
+
+class _MyProjectPageState extends State<MyProjectPage> {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+    );
+  }
+}
