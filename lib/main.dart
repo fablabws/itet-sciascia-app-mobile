d4b64e7356c44dfd0eda9282725e6b67e88f8c49
Enrico La Sala <enrico.lasala@buildnn.com>
2022-05-16 16:08:05 +0200

    Modificato Colore

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ITET Sciascia App Mobile',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: MyHomePage(),
     );
